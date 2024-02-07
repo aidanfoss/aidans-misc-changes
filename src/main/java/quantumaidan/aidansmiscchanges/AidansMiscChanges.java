5061d3db098c8628538f750b02e8cfae0a1fdce9
Aidan <quantumaidan@gmail.com>
2024-02-07 03:10:08 -0600

    added customDrops call and potential for toggle later using settings

--- src/main/java/quantumaidan/aidansmiscchanges/AidansMiscChanges.java
+++ src/main/java/quantumaidan/aidansmiscchanges/AidansMiscChanges.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import quantumaidan.aidansmiscchanges.util.CustomDrops;
 import quantumaidan.aidansmiscchanges.util.CustomTrades;
 
 public class AidansMiscChanges implements ModInitializer {
@@ -14,13 +15,18 @@ public class AidansMiscChanges implements ModInitializer {
 		//change this to call from config file if I ever figure that out.
 		boolean customTradesToggle = true;
 		boolean customCraftsToggle = true;
+		boolean customsDropsToggle = true;
 
 		if (customTradesToggle){
 			CustomTrades.registerCustomTrades();
 		}
 		if (customCraftsToggle){
+			//not sure if this is even possible to toggle tbh
 			//9 string for a cobweb
 			//wool block in the center for a dispenser
+		}
+		if (customsDropsToggle){
+			CustomDrops.addAll();
 		}
 
 		LOGGER.info("Hello Fabric world!");
